a3b67db554e3cf108e968e5efe2984081b1d6030
jeyofdev <jgregoire.pro@gmail.com>
2020-12-20 15:48:20 +0100

    style : define the css of the top nav

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,9 @@
 import '../styles/app.scss';
+
+const btnTrigger = document.getElementById('btn-trigger');
+const collapse = document.getElementById('collapse');
+
+btnTrigger.addEventListener('click', () => {
+  btnTrigger.classList.toggle('is-active');
+  collapse.classList.toggle('is-open');
+});
